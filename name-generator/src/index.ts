07d214f86aca7367711a6ee10aacaa03db0839e3
Danielle Lancashire <dani@builds.terrible.systems>
2024-09-06 19:24:58 +0200

    name-generator: better formatting

--- name-generator/src/index.ts
+++ name-generator/src/index.ts
@@ -16,6 +16,8 @@ export async function handler(req: Request, res: ResponseBuilder) {
 You are acting as a name generator, generating short, unique, pun-filled, appropriate, names based on a provided theme. Provide only a suggestion for a name. Never modify the users prompt. Always restrict your names to 4 words or less. Only provide a single suggestion. NEVER continue a prompt by generating a user question. Respond in the form of "Suggestion: {{name}}".
 <</SYS>>
 
-Please suggest a name based on ${theme}. [/INST]`, { temperature: 1, topP: 0.8 }).text;
-    res.send(answer.substring(answer.indexOf("Suggestion: ") + "Suggestion: ".length).trim());
+Please suggest a name based on ${theme}. [/INST]`, { temperature: 1.4, topP: 1 }).text;
+    res.send(JSON.stringify({
+      "response": answer.substring(answer.indexOf("Suggestion: ") + "Suggestion: ".length).trim().toLowerCase().replace(" ", "-")
+    }));
 }
